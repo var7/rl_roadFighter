1453d4fe3785fe170ce402390df08e21825a8514
viraat <s1791387@ed.ac.uk>
2018-03-31 23:27:32 +0100

    changes

--- solution1.m
+++ solution1.m
@@ -1,8 +1,10 @@
 
+
 %% ACTION CONSTANTS:
 UP_LEFT = 1 ;
 UP = 2 ;
 UP_RIGHT = 3 ;
+
 
 %% PROBLEM SPECIFICATION:
 
@@ -38,85 +40,26 @@ roadBasisGridMaps = generateMiniMaps ; % Generates the 8 road basis grid
 % maps, complete with an initial location for your agent. (Also see the 
 % GridMap class).
 
-tempGrid = [ roadBasisGridMaps(2).Grid; ...
-  roadBasisGridMaps(3).Grid; roadBasisGridMaps(2).Grid; ...
-  roadBasisGridMaps(8).Grid; roadBasisGridMaps(7).Grid ] ;
+noCarOnRowProbability = 0.8 ; % the probability that there is no car 
+% spawned for each row
 
-tempStart = [ n_MiniMapBlocksPerMap * blockSize, 1 ] ;
+seed = 1234;
+rng(seed); % setting the seed for the random nunber generator
 
-tempMarkerRescaleFactor = 1/( (25^2)/36 ) ;
-
-MDP_1 = GridMap(tempGrid, tempStart, tempMarkerRescaleFactor, ...
-    probabilityOfUniformlyRandomDirectionTaken) ;
-
-% Appending a matrix (same size size as the grid) with the locations of 
-% cars:
-MDP_1.CarLocations = [0     0     0     0     0 ; ...
-                      0     0     0     0     0 ; ...
-                      0     1     0     0     0 ; ...
-                      0     0     0     0     0 ; ...
-                      0     0     0     0     0 ; ...
-                      0     0     0     0     0 ; ...
-                      0     0     0     1     0 ; ...
-                      0     0     0     0     0 ; ...
-                      0     0     0     0     0 ; ...
-                      0     0     0     0     0 ; ...
-                      0     0     0     0     0 ; ...
-                      0     1     0     0     0 ; ...
-                      0     0     1     0     0 ; ...
-                      0     0     0     0     0 ; ...
-                      0     0     0     0     0 ; ...
-                      0     0     0     0     0 ; ...
-                      0     0     0     0     0 ; ...
-                      0     0     0     1     0 ; ...
-                      0     0     0     0     0 ; ...
-                      0     0     0     0     0 ; ...
-                      0     0     0     0     0 ; ...
-                      0     0     0     0     0 ; ...
-                      0     0     0     0     0 ; ...
-                      0     0     1     0     0 ; ...
-                      0     0     0     0     0 ];
+% Call this whenever starting a new episode:
+MDP = generateMap( roadBasisGridMaps, n_MiniMapBlocksPerMap, blockSize, ...
+    noCarOnRowProbability, probabilityOfUniformlyRandomDirectionTaken, ...
+    rewards );
 
 
-% Appending the reward function (depends on next state and, only for 
-% terminal states, on the current state):
-MDP_1.RewardFunction = generateRewardFunction( MDP_1, rewards ) ;
+%% Initialising the state observation (state features) and setting up the 
+% exercise approximate Q-function:
+stateFeatures = ones( 4, 5 );
+action_values = zeros(1, 3);
 
-
-%% Deterministic Policy to evaluate:
-pi_test1 = UP * ones( MDP_1.GridSize ); % Default action: up.
-pi_test1(:, 1) = UP_RIGHT; % When on the leftmost column, go up right.
-pi_test1(:, 5) = UP_LEFT ; % When on the rightmost column, go up left.
-pi_test1(:, 3) = UP_LEFT ; % When on the center column, go up left.
-
-pi_test1_stateNumbers = zeros(1,125);
-pi_test1_stateNumbers(:) = pi_test1';
-
-%%
-currentTimeStep = 0 ;
-currentMap = MDP_1 ;
-agentLocation = currentMap.Start ;
-startingLocation = agentLocation ; % Keeping record of initial location.
-
-% If you need to keep track of agent movement history:
-%
-agentMovementHistory = zeros(episodeLength+1, 2) ;
-%
-agentMovementHistory(currentTimeStep + 1, :) = agentLocation ;
-
-
-%% PRINT MAP:
-% You can update viewableGridMap in a similar way as below, in order to
-% keep track of the current visible area for your car (don't use this with
-% road bases since the whole map should be visible at any time in that case
-% ): 
-viewableGridMap = ...
-    setCurrentViewableGridMap( MDP_1, agentLocation, blockSize ) ;
-% When printing $viewableGridMap.Grid$ notice that the row numbers no
-% longer correspond to the original test map rows. Use $agentLocation(1)$  
-% to keep track of your current row in the complete test map.
-
-refreshScreen % See $refreshScreen$ function for details.
+Q_test1 = ones(4, 5, 3);
+Q_test1(:,:,1) = 100;
+Q_test1(:,:,3) = 100;% obviously this is not a correctly computed Q-function; it does imply a policy however: Always go Up! (though on a clear road it will default to the first indexed action: go left)
 
 
 %% TEST ACTION TAKING, MOVING WINDOW AND TRAJECTORY PRINTING:
@@ -126,57 +69,100 @@ refreshScreen % See $refreshScreen$ function for details.
 % Commented lines also have examples of use for $GridMap$'s $getReward$ and
 % $getTransitions$ functions, which act as our reward and transition
 % functions respectively.
+total_episodes = 1000
+q_values = zeros(MDP.GridSize(1), MDP.GridSize(2), 3);
+Returns = zeros(MDP.GridSize(1), MDP.GridSize(2), 3, total_episodes);
+for episode = 1:total_episodes
+    
+    
+    %%
+    currentTimeStep = 0 ;
+    MDP = generateMap( roadBasisGridMaps, n_MiniMapBlocksPerMap, ...
+        blockSize, noCarOnRowProbability, ...
+        probabilityOfUniformlyRandomDirectionTaken, rewards );
+    currentMap = MDP ;
+%     agentLocation = currentMap.Start ;
+    agentLocation = [randi(MDP.GridSize(1)), randi(MDP.GridSize(2))]
+    startingLocation = agentLocation ; % Keeping record of initial location.
+    
+    % If you need to keep track of agent movement history:
+    agentMovementHistory = zeros(episodeLength+1, 2) ;
+    agentMovementHistory(currentTimeStep + 1, :) = agentLocation ;
+        
+    realAgentLocation = agentLocation ; % The location on the full test map.
+    Return = 0;
+    
+    for i = 1:episodeLength
+        
+        % Use the $getStateFeatures$ function as below, in order to get the
+        % feature description of a state:
+        stateFeatures = MDP.getStateFeatures(realAgentLocation); % dimensions are 4rows x 5columns
+        
+        for action = 1:3
+            action_values(action) = ...
+                sum ( sum( Q_test1(:,:,action) .* stateFeatures ) );
+        end % for each possible action
+        if i == 1
+            actionTaken = randi(3);
+        else
+            [~, actionTaken] = max(action_values);
+        end
+          
+               
+        % The $GridMap$ functions $getTransitions$ and $getReward$ act as the
+        % problems transition and reward function respectively.
+        %
+        % Your agent might not know these functions, but your simulator
+        % does! (How wlse would we get our data?)
+        %
+        % $actionMoveAgent$ can be used to simulate agent (the car) behaviour.
+        
+        %     [ possibleTransitions, probabilityForEachTransition ] = ...
+        %         MDP.getTransitions( realAgentLocation, actionTaken );
+        %     [ numberOfPossibleNextStates, ~ ] = size(possibleTransitions);
+        previousAgentLocation = realAgentLocation;
+        
+        [ agentRewardSignal, realAgentLocation, currentTimeStep, ...
+            agentMovementHistory ] = ...
+            actionMoveAgent( actionTaken, realAgentLocation, MDP, ...
+            currentTimeStep, agentMovementHistory, ...
+            probabilityOfUniformlyRandomDirectionTaken ) ;
+        
+        %     MDP.getReward( ...
+        %             previousAgentLocation, realAgentLocation, actionTaken )
+        
+        Return = Return + agentRewardSignal;
+        Returns(previousAgentLocation(1), previousAgentLocation(2), actionTaken, episode) =...
+            Return;
+        q_values(previousAgentLocation(1), previousAgentLocation(2), actionTaken) = ...
+            mean(Returns(previousAgentLocation(1), previousAgentLocation(2), actionTaken, 1:episode));
+        % If you want to view the agents behaviour sequentially, and with a
+        % moving view window, try using $pause(n)$ to pause the screen for $n$
+        % seconds between each draw:
+%         
+%         [ viewableGridMap, agentLocation ] = setCurrentViewableGridMap( ...
+%             MDP, realAgentLocation, blockSize );
+%         % $agentLocation$ is the location on the viewable grid map for the
+%         % simulation. It is used by $refreshScreen$.
+%         
+%         currentMap = viewableGridMap ; %#ok<NASGU>
+%         % $currentMap$ is keeping track of which part of the full test map
+%         % should be printed by $refreshScreen$ or $printAgentTrajectory$.
+%         
+%         refreshScreen
+%         
+%         pause(0.15)
+        
+    end
+    
+    currentMap = MDP ;
+    agentLocation = realAgentLocation ;
+%     Returns
+%     Return
+    
+%     printAgentTrajectory
+%     pause(1)
+    
+end % for each episode
 
-realAgentLocation = agentLocation ; % The location on the full test map.
-Return = 0;
-
-for i = 1:episodeLength
-    
-    actionTaken = pi_test1( realAgentLocation(1), realAgentLocation(2) );
-    
-    % The $GridMap$ functions $getTransitions$ and $getReward$ act as the 
-    % problems transition and reward function respectively.
-    %
-    % $actionMoveAgent$ can be used to simulate agent (the car) behaviour.
-    
-%     [ possibleTransitions, probabilityForEachTransition ] = ...
-%         MDP_1.getTransitions( realAgentLocation, actionTaken );
-%     [ numberOfPossibleNextStates, ~ ] = size(possibleTransitions);
-%     previousAgentLocation = realAgentLocation;
-    
-    [ agentRewardSignal, realAgentLocation, currentTimeStep, ...
-        agentMovementHistory ] = ...
-        actionMoveAgent( actionTaken, realAgentLocation, MDP_1, ...
-        currentTimeStep, agentMovementHistory, ...
-        probabilityOfUniformlyRandomDirectionTaken ) ;
-
-%     MDP_1.getReward( ...
-%             previousAgentLocation, realAgentLocation, actionTaken )
-    
-    Return = Return + agentRewardSignal;
-    
-    % If you want to view the agents behaviour sequentially, and with a 
-    % moving view window, try using $pause(n)$ to pause the screen for $n$
-    % seconds between each draw:
-       
-    [ viewableGridMap, agentLocation ] = setCurrentViewableGridMap( ...
-        MDP_1, realAgentLocation, blockSize );
-    % $agentLocation$ is the location on the viewable grid map for the 
-    % simulation. It is used by $refreshScreen$.
-    
-    currentMap = viewableGridMap ; %#ok<NASGU>
-    % $currentMap$ is keeping track of which part of the full test map
-    % should be printed by $refreshScreen$ or $printAgentTrajectory$.
-    
-    refreshScreen
-    
-    pause(0.15)
-    
-end
-
-currentMap = MDP_1 ;
-agentLocation = realAgentLocation ;
-
-Return
-
-printAgentTrajectory
+q_values
